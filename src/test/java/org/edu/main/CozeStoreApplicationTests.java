052e604456776a45130e29bf2730012ac1a9899c
Admin <nguyenlanh121004@gmail.com>
2025-01-25 17:04:59 +0700

    setup project

---
+++ src/test/java/org/edu/main/CozeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edu.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozeStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
